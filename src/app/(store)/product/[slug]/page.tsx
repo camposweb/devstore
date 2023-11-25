48b29450ff4de6ad1ce4f66cadb379dc7aa19bf7
Leandro Campos <camposweb@gmail.com>
2023-11-25 15:34:23 -0300

    OpenGraphImage and AddToCartButton

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from '@/components/AddToCartButton'
 import { api } from '@/lib/api'
 import { Product } from '@/types/product'
 import { Metadata } from 'next'
@@ -69,6 +70,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
+            Em até 12x s/juros de {''}
             {(product.price / 12).toLocaleString('pt-BR', {
               style: 'currency',
               currency: 'BRL',
@@ -106,13 +108,7 @@ export default async function ProductPage({ params }: ProductProps) {
             </button>
           </div>
         </div>
-
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   )
--- src/contexts/CarContext.tsx
+++ src/contexts/CarContext.tsx
@@ -3,13 +3,13 @@
 import { ReactNode, createContext, useContext, useState } from 'react'
 
 interface CartItem {
-  productId: string
+  productId: number
   quantity: number
 }
 
 interface CarContextType {
   items: CartItem[]
-  addToCart: (productId: string) => void
+  addToCart: (productId: number) => void
 }
 
 const CartContext = createContext({} as CarContextType)
@@ -21,7 +21,7 @@ interface CardProviderProps {
 export function CartProvider({ children }: CardProviderProps) {
   const [cartItems, setCartItems] = useState<CartItem[]>([])
 
-  function addToCart(productId: string) {
+  function addToCart(productId: number) {
     setCartItems((state) => {
       const productInCart = state.some((item) => item.productId === productId)
 
