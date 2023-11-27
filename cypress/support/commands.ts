751a4e2076ca08fef631cfcddde0cd6284763623
Leandro Campos <camposweb@gmail.com>
2023-11-26 22:12:18 -0300

    Custom test and CI Github actions

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,7 +11,18 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+
+declare namespace Cypress {
+  interface Chainable {
+    searchByQuery(query: string): Chainable<void>
+  }
+}
+
+Cypress.Commands.add('searchByQuery', (query: string) => {
+  cy.visit('/')
+
+  cy.get('input[name=q]').type(query).parent('form').submit()
+})
 //
 //
 // -- This is a child command --
@@ -25,13 +36,3 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
